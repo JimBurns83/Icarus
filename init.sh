b9c431c5648539c6e9aa035d7a49db072dfb5b4f
JimBurnsNice <55092215+JimBurnsNice@users.noreply.github.com>
2020-05-22 22:42:52 +0100

    fix sudo user

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ apt-get update
 apt-get install qrencode python3-pip -y
 pip3 install ansible
 #ln -s -f /usr/local/bin/ansible /usr/bin/ansible
-ansible-playbook $PWD/Icarus/Ansible/icarus_playbook.yaml
+ansible-playbook Icarus/Ansible/icarus_playbook.yaml
 #/home/jim/github.com/JimBurns83
