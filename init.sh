5a713f415fa8b1dd9e87dd4d1498c431b8b2e609
JimBurnsNice <55092215+JimBurnsNice@users.noreply.github.com>
2020-05-22 22:28:47 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@ apt-get update
 apt-get install qrencode python3-pip -y
 pip3 install ansible
 #ln -s -f /usr/local/bin/ansible /usr/bin/ansible
-ansible-playbook ~/github.com/JimBurns83/Icarus/Ansible/icarus_playbook.yaml
+ansible-playbook $PWD/Icarus/Ansible/icarus_playbook.yaml
+#/home/jim/github.com/JimBurns83
