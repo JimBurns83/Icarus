c45f0df10b0c8d1820ab223bf214dbff7669949a
JimBurnsNice <55092215+JimBurnsNice@users.noreply.github.com>
2020-05-24 15:52:04 +0100

    Move Hephaestus to own repo

--- init.sh
+++ init.sh
@@ -6,5 +6,6 @@ apt-get install qrencode python3-pip -y
 pip3 install ansible
 #ln -s -f /usr/local/bin/ansible /usr/bin/ansible
 ansible-playbook Icarus/Ansible/icarus_playbook.yaml
+ansible-playbook Hephaestus/Ansible/Launchpad/init_playbook.yaml
 #Hephaestus/Init.sh
 #/home/jim/github.com/JimBurns83
