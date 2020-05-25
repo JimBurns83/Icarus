53859de58c73cba8b3452b2202a78c106b358480
JimBurnsNice <55092215+JimBurnsNice@users.noreply.github.com>
2020-05-25 19:14:42 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ apt-get install qrencode python3-pip -y
 pip3 install ansible
 #ln -s -f /usr/local/bin/ansible /usr/bin/ansible
 ansible-playbook Icarus/Ansible/icarus_playbook.yaml
-ansible-playbook Hephaestus/Ansible/Launchpad/init_playbook.yaml
+#ansible-playbook Hephaestus/Ansible/Launchpad/init_playbook.yaml
+ansible-playbook Hephaestus/Ansible/Launchpad/play_local_launchpad.yaml
+ansible-playbook Hephaestus/Ansible/Launchpad/play_local_gantry.yaml
 #Hephaestus/Init.sh
 #/home/jim/github.com/JimBurns83
